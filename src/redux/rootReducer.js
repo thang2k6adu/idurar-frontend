fea8c5f456d779c390f39741b3649e5a0c47d84f
thang2k6adu <thang2k6adu@gmail.com>
2025-08-18 07:14:59 +0700

    feature: erp reducer
    -changes:
      - types, actions, reducer, selector
    
    - details:
      - This reducer is used for managing state operation of entitied like invoice, quote and payment
      - format:
        - current: current item
        - list: Manage list of items with pagination
        - create/update/delete/read: operations CRUD with loading and success state
        - recordPayment: processomg payment for invoice
        - search/summary/mail: additional features
    - actionType:
      - RESET_STATE: reset all state to INITIAL_STATE
      - REQUEST_LOADING/SUCCESS/FAILED: operation state
      - CURREN_ACTION/RESET_ACTION: managing certain action state

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,25 +1,27 @@
 import { combineReducers } from 'redux'
 import { reducer as authReducer } from './auth'
 import { reducer as settingsReducer } from './settings'
+import { reducer as erpReducer } from './erp'
 
 // {
 //  auth:{
 //  }
 // ...
-  // settings: {
-  //   result: {
-  //     money_format_settings: {
-  //       decimal_places: 2,
-  //       currency_symbol: '$',
-  //     },
-  //   },
-  //   isLoading: false,
-  //   isSuccess: true,
-  // },
+// settings: {
+//   result: {
+//     money_format_settings: {
+//       decimal_places: 2,
+//       currency_symbol: '$',
+//     },
+//   },
+//   isLoading: false,
+//   isSuccess: true,
+// },
 // }
 const rootReducer = combineReducers({
   auth: authReducer,
   settings: settingsReducer,
+  erp: erpReducer,
 })
 
 export default rootReducer
