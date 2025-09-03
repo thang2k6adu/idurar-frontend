0bb6366e6d649ef32b9942034d4de27e550dbf57
thang2k6adu <thang2k6adu@gmail.com>
2025-09-03 10:01:52 +0700

    fix: api base url

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE === 'remote'
-    ? import.meta.env.VITE_BACKEND_SERVER
+    ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
     : 'http://localhost:8888/api/'
 
 export const BASE_URL =
