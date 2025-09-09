c213c9e5e6197adcdd456ae7b9aa3e90f311ff76
thang2k6adu <thang2k6adu@gmail.com>
2025-09-09 22:36:51 +0700

    minor change

--- src/forms/loginForm.jsx
+++ src/forms/loginForm.jsx
@@ -47,7 +47,7 @@ export default function LoginForm() {
         <Form.Item name="remember" valuePropName="checked" noStyle>
           <Checkbox>{translate('Remember me')}</Checkbox>
         </Form.Item>
-        <a className="login-form-forgot" href="/forgetpassword" style={{ marginLeft: '0px' }}>
+        <a className="login-form-forgot" href="/forgetPassword" style={{ marginLeft: '0px' }}>
           {translate('Forgot password')}
         </a>
       </Form.Item>
